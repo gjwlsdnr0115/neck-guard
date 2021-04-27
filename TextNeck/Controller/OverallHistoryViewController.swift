7b855b4f113de37c99a11fad6166e678d7e6bd34
Jinwook <gjwlsdnr0115@gmail.com>
2021-04-27 18:14:26 +0900

    Update score tab UI

--- TextNeck/Controller/OverallHistoryViewController.swift
+++ TextNeck/Controller/OverallHistoryViewController.swift
@@ -13,10 +13,28 @@ class OverallHistoryViewController: UIViewController {
     @IBOutlet weak var weeklyButton: UIButton!
     @IBOutlet weak var monthlyButton: UIButton!
     
+    
+    
+    
+    
+    
+    
+    @IBOutlet weak var todayScoreCircleChartView: UIView!
+    
+    
+    
+    
+    
+    
+    let values = [0.8]
+    let chartColor = [UIColor(displayP3Red: 124/255.0, green: 225/255.0, blue: 238/255.0, alpha: 1.0),
+                      UIColor(displayP3Red: 73/255.0, green: 60/255.0, blue: 199/255.0, alpha: 1.0),]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateScoreTab()
     }
     
 
@@ -34,4 +52,10 @@ class OverallHistoryViewController: UIViewController {
             weeklyButton.isSelected = false
         }
     }
+    
+    
+    func updateScoreTab() {
+        drawCircleChart(values: values, fgColor: chartColor[0], bgColor: chartColor[1], width: 20, margin: 2, radius: 34, view: todayScoreCircleChartView)
+    }
+    
 }
